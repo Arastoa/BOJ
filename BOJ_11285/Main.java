542ecee95bf16b1f68bcd37aaef52f47d4a77a7b
Arastoa <waninokow@gmail.com>
2015-10-17 17:12:09 +0900

    Create Main.java

---
+++ BOJ_11285/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String cho[]={"ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ" };
+		String joong[]={"ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"};
+		String jong[]={"", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"};
+		String str=scan.nextLine();
+		int u=Arrays.binarySearch(cho, str)*21*28;
+		str=scan.nextLine();
+		u+=Arrays.binarySearch(joong, str)*28;
+		str=scan.nextLine();
+		u+=Arrays.binarySearch(jong, str);
+		
+		u+=0xac00;
+		System.out.println((char)u);
+	}
+
+}
