18338f6a8b6a8a38f7c7ff0085c54a9742580e82
Arastoa <waninokow@gmail.com>
2017-08-28 22:46:39 +0900

    Create Solution6.cpp

---
+++ Kakao/practice/Solution6.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <string.h>
+using namespace std;
+
+int cache[100000][4][2];
+int n;
+
+int DPSol(const vector<int>& sticker, int pos, int offset, bool isSelectFirst){
+    if(pos>=n) return 0;
+    if(pos == n-1){
+        if(isSelectFirst)
+             return 0;
+        return sticker[pos];
+    }
+    int& ret = cache[pos][offset][(int)isSelectFirst];
+    if(ret != -1) return ret;
+    
+    ret = 0;
+    ret = max(ret, DPSol(sticker, pos + 2, 1, isSelectFirst) + sticker[pos]);  
+    ret = max(ret, DPSol(sticker, pos + 3, 2, isSelectFirst) + sticker[pos]);
+    
+    return ret;
+}
+
+int solution(vector<int> sticker)
+{
+	int answer = 0;
+    memset(cache, -1, sizeof(cache));
+    n = sticker.size();
+    answer = max(DPSol(sticker, 0, 0, true), DPSol(sticker, 1, 0, false));
+    answer = max(DPSol(sticker, 2, 0, false), answer);
+    
+	return answer;
+}
